e490e6f63f57c0515eb416378f97bb3759d38756
GabrielTSR <gabrielalvesrocha22@gmail.com>
2021-11-05 10:59:40 -0300

    funcoes de deletar, ler por id, e editar atualizados

--- v1/crud.php
+++ v1/crud.php
@@ -18,6 +18,37 @@ function read($conn){
     }
 }
 
+# FUNÇÃO DE LEITURA DE DADOS (COM BASE NO ID)
+function readID($cod_pessoa, $conn){
+    $sql = "SELECT * FROM tbl_pessoa WHERE cod_pessoa=$cod_pessoa";
+
+    if ($resultado = mysqli_query($conn, $sql)) {
+        $dados = mysqli_fetch_assoc($resultado);
+
+        echo json_encode(['status' => 'sucess', 'data'=> $dados]);
+    } else {
+        echo json_encode(['status'=>'error', 'data'=>mysqli_error($conn)]);
+    }
+}
+
+#FUNÇÃO DE ATUALIZAÇÃO
+function update($cod_pessoa, $nome, $sobrenome, $email, $celular, $fotografia, $conn){
+
+    $sql = "UPDATE tbl_pessoa SET
+            nome = '$nome',
+            sobrenome = '$sobrenome',
+            email = '$email',
+            celular = '$celular',
+            fotografia = '$fotografia'
+            WHERE cod_pessoa = $cod_pessoa";
+
+    if (mysqli_query($conn, $sql)) {
+        echo json_encode(array('status'=>'sucess', 'data'=>'Dados alterados com sucesso!'));
+    } else {
+        echo json_encode(array('status'=>'error', 'data'=>mysqli_error($conn)));
+    }
+}
+
 #FUNÇÃO DE INSERÇÃO
 function create($nome, $sobrenome, $email, $celular, $fotografia, $conn){
     $sql = "INSERT INTO tbl_pessoa (nome, sobrenome, email, celular, fotografia)
@@ -27,6 +58,18 @@ function create($nome, $sobrenome, $email, $celular, $fotografia, $conn){
         echo json_encode(array('status'=>'sucess', 'data'=>'Dados inseridos com sucesso'));
     } else {
         echo json_encode(array('status'=>'error', 'data'=>'Erro ao inserir os dados'));
+    }
+}
+
+#FUNÇÃO DE EXCLUSÃO
+
+function delete($cod_pessoa, $conn){
+    $sql = "DELETE FROM tbl_pessoa WHERE cod_pessoa = $cod_pessoa";
+
+    if (mysqli_query($conn, $sql)) {
+        echo json_encode(array('status'=>'sucess', 'data'=>'Dados deletados com sucesso'));
+    } else {
+        echo json_encode(array('status'=>'error', 'data'=>'Erro ao deletar os dados'));
     }
 }
 
--- v1/index.php
+++ v1/index.php
@@ -14,6 +14,11 @@
             read($conn);
             break;
 
+        case 'readID':
+            $cod_pessoa = $_REQUEST['cod_pessoa'];
+            readID($cod_pessoa, $conn);
+            break;
+
         case 'create':
             $nome = $_REQUEST['nome'];
             $sobrenome = $_REQUEST['sobrenome'];
@@ -22,6 +27,23 @@
             $fotografia = $_REQUEST['fotografia'];
 
             create($nome, $sobrenome, $email, $celular, $fotografia, $conn);
+            break;
+
+        case 'update':
+            $cod_pessoa = $_REQUEST['cod_pessoa'];
+            $nome = $_REQUEST['nome'];
+            $sobrenome = $_REQUEST['sobrenome'];
+            $email = $_REQUEST['email'];
+            $celular = $_REQUEST['celular'];
+            $fotografia = $_REQUEST['fotografia'];
+
+            update($cod_pessoa, $nome, $sobrenome, $email, $celular, $fotografia, $conn);
+            break;
+
+        case 'delete':
+            $cod_pessoa = $_REQUEST['cod_pessoa'];
+
+            delete($cod_pessoa, $conn);
             break;
         
         default:
